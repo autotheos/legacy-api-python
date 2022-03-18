e7634c297555cf954979241ba8d4b39406a51348
autotheos <59674770+autotheos@users.noreply.github.com>
2022-03-17 21:51:28 -0700

    Update find-transaction.py
    change tron to lgcy

--- examples/find-transaction.py
+++ examples/find-transaction.py
@@ -1,13 +1,13 @@
-from tronapi import Tron
-from tronapi import HttpProvider
+from legacyapi import Legacy
+from legacyapi import HttpProvider
 
-full_node = HttpProvider('https://api.trongrid.io')
-solidity_node = HttpProvider('https://api.trongrid.io')
-event_server = HttpProvider('https://api.trongrid.io')
-tron = Tron(full_node=full_node,
+full_node = HttpProvider('https://apilg.lgcyscan.network/')
+solidity_node = HttpProvider('https://apilg.lgcyscan.network/')
+event_server = HttpProvider('https://apilg.lgcyscan.network/')
+legacy = Legacy(full_node=full_node,
             solidity_node=solidity_node,
             event_server=event_server)
 
 
 
-result = tron.trx.get_transaction('TxId')
+result = legacy.lgcy.get_transaction('TxId')
