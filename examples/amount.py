6151f02c2589e570041818e448d01eee162cdf55
autotheos <59674770+autotheos@users.noreply.github.com>
2022-03-17 21:47:07 -0700

    Update amount.py
    change tron to lgcy

--- examples/amount.py
+++ examples/amount.py
@@ -1,19 +1,19 @@
-from tronapi import Tron
-from tronapi import HttpProvider
+from legacyapi import Legacy
+from legacyapi import HttpProvider
 
-full_node = HttpProvider('https://api.trongrid.io')
-solidity_node = HttpProvider('https://api.trongrid.io')
-event_server = HttpProvider('https://api.trongrid.io')
-tron = Tron(full_node=full_node,
+full_node = HttpProvider('https://apilg.lgcyscan.network/')
+solidity_node = HttpProvider('https://apilg.lgcyscan.network/')
+event_server = HttpProvider('https://apilg.lgcyscan.network/')
+legacy = Legacy(full_node=full_node,
             solidity_node=solidity_node,
             event_server=event_server)
 
 
 
-tron.toSun(1)
+legacy.toSun(1)
 # result: 1000000
 
-tron.fromSun(1000000)
+legacy.fromSun(1000000)
 # result: 1
 
 
