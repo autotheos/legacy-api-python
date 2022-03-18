35e145d24d613076b68a4b1fe3237a4b87d3040b
autotheos <59674770+autotheos@users.noreply.github.com>
2022-03-17 21:53:16 -0700

    Update send-transaction.py
    change tron to lgcy api

--- examples/send-transaction.py
+++ examples/send-transaction.py
@@ -1,19 +1,19 @@
-from tronapi import Tron
-from tronapi import HttpProvider
+from legacyapi import Legacy
+from legacyapi import HttpProvider
 
-full_node = HttpProvider('https://api.trongrid.io')
-solidity_node = HttpProvider('https://api.trongrid.io')
-event_server = HttpProvider('https://api.trongrid.io')
+full_node = HttpProvider('https://apilg.lgcyscan.network/')
+solidity_node = HttpProvider('https://apilg.lgcyscan.network/')
+event_server = HttpProvider('https://apilg.lgcyscan.network/')
 
-tron = Tron(full_node=full_node,
+legacy = Legacy(full_node=full_node,
             solidity_node=solidity_node,
             event_server=event_server)
 
 
-tron.private_key = 'private_key'
-tron.default_address = 'default address'
+legacy.private_key = 'private_key'
+legacy.default_address = 'default address'
 
 # added message
-send = tron.trx.send_transaction('to', 1)
+send = legacy.lgcy.send_transaction('to', 1)
 
 print(send)
