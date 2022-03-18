daa630180ea863d7b76690800f525e995337fad6
autotheos <59674770+autotheos@users.noreply.github.com>
2022-03-17 21:44:30 -0700

    Update address-hex.py
    change tron to lgcy api

--- examples/address-hex.py
+++ examples/address-hex.py
@@ -1,16 +1,16 @@
-from tronapi import Tron
-from tronapi import HttpProvider
+from legacyapi import Legacy
+from legacyapi import HttpProvider
 
-full_node = HttpProvider('https://api.trongrid.io')
-solidity_node = HttpProvider('https://api.trongrid.io')
-event_server = HttpProvider('https://api.trongrid.io')
-tron = Tron(full_node=full_node,
+full_node = HttpProvider('https://apilg.lgcyscan.network/')
+solidity_node = HttpProvider('https://apilg.lgcyscan.network/')
+event_server = HttpProvider('https://apilg.lgcyscan.network/')
+legacy = Legacy(full_node=full_node,
             solidity_node=solidity_node,
             event_server=event_server)
 
 
-tron.address.to_hex('TT67rPNwgmpeimvHUMVzFfKsjL9GZ1wGw8')
+legacy.address.to_hex('TT67rPNwgmpeimvHUMVzFfKsjL9GZ1wGw8')
 # result: 41BBC8C05F1B09839E72DB044A6AA57E2A5D414A10
 
-tron.address.from_hex('41BBC8C05F1B09839E72DB044A6AA57E2A5D414A10')
+legacy.address.from_hex('41BBC8C05F1B09839E72DB044A6AA57E2A5D414A10')
 # result: TT67rPNwgmpeimvHUMVzFfKsjL9GZ1wGw8
